cffa0b3a8be37bee7b5f39112660c23d7ab7c197
Oliver Vuorenmaa <vuoreol@protonmail.com>
2022-11-01 17:09:48 +0200

    Yeah

--- photo_gallery/photo_gallery/urls.py
+++ photo_gallery/photo_gallery/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('photos/', include('photos.urls')),
     path('admin/', admin.site.urls),
+    path('galleries/', include('galleries.urls')),
 ]
