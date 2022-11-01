69fb51cd52b96e33133c9e2aad1b285e35aef440
Oliver Vuorenmaa <vuoreol@protonmail.com>
2022-11-01 16:27:21 +0200

    Added migrations

--- photo_gallery/photos/models.py
+++ photo_gallery/photos/models.py
@@ -3,5 +3,5 @@ from galleries.models import Gallery
 
 class Photo(models.Model):
     photo = models.ImageField(upload_to='photos')
-    gallery = models.ForeignKey(Gallery, ondelete=models.CASCADE)
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
     info_text = models.CharField(max_length=20)
