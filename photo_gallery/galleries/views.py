dc727d4991ecc2a2aef9a765a7ecf41b25dd8aa1
Oliver Vuorenmaa <vuoreol@protonmail.com>
2022-11-01 16:59:26 +0200

    Added galleries template

--- photo_gallery/galleries/views.py
+++ photo_gallery/galleries/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Gallery
 
-# Create your views here.
+def index(request):
+    galleries_list = Gallery.objects.all().filter(private=False)
+    template = loader.get_template('galleries/index.html')
+    
+    context = {
+        'galleries_list': galleries_list,
+    }
+    return HttpResponse(template.render(context, request))
